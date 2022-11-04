e28000e3b88d44c69790556cab541539e163a0e2
abu-desai <abudesai@live.com>
2022-11-04 15:04:51 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IssueCreateComponent } from './issue/issue-create/issue-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssueCreateComponent
   ],
   imports: [
     BrowserModule
